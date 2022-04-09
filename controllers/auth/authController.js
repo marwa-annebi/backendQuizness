8efc9c9f71a036c4c9758345aff3115706b64731
hadil30 <hedilfares30@gmail.com>
2022-04-09 16:29:43 +0100

    fixedToken

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -7,8 +7,9 @@ const Candidate = require("../../models/users/candidateModel");
 const QuizMaster = require("../../models/users/quizMasterModel");
 const UserOtpVerification = require("../../models/users/userOtpVerification");
 const bcrypt = require("bcryptjs");
+var jwt = require('jsonwebtoken');
 const myEnum = require("./enumUser");
-const tokenModel = require("../../models/users/tokenModel");
+const  generateToken = require("../../utils/generateToken");
 const crypto =require ("crypto");
 const registerAdmin = asyncHandler(async (req, res) => {
   try {
@@ -245,83 +246,78 @@ const resendverification = asyncHandler(async (req, res) => {
 
 //login user
 
-const login = asyncHandler(async (req, res) => {
-  
-  try{
-  const {email,password } = req.body;
-  switch (req.params.typeUser) {
-    case myEnum.ADMIN.value:
-      user = await Admin.findOne({
-        email,
+const loginUser = asyncHandler(async (req, res) => {
+  try {
+   
+    const {email,password,type} = req.body;
+    switch (type) {
+      case myEnum.ADMIN.value:
+        user = await Admin.findOne({
+          email,
+        });
+        // console.log(user);
+        if (!user) {
+          res.json({
+            message: "user doesn't exist",
+          });
+        }
+        // console.log(user);
+        else if (user) {
+          if (await user.matchPassword(req.body.password)) {
+            return  res.json({
+              email,
+              password,
+            });
+          } else {
+           return   res.json({
+              message: "Invalid email or password",
+            });
+          }
+        }
+        break;
+      case myEnum.CANDIDATE.value:
+        user = await Candidate.findOne({
+          email,
+        });
+        break;
+      case myEnum.QUIZMASTER.value:
+        user = await QuizMaster.findOne({
+          email: req.body.email,
+        });
+        // console.log(user);
+        break;
+      default:
+        throw console.error("user doesn't exist");
+       
+    }
+     if (!user) {
+     return   res.status(404).json({
+        message: "user doesn't exist",
       });
-       console.log(user);
-      if (!user) {
-        res.json({
-          message: "user doesn't exist",
+      
+    }
+
+    //console.log(user);
+     else if (user) {
+     console.log(user);
+      if (!user.verified) {
+        return  res.status(400).json({
+          message: "Please verify your account",
+        });
+      } else if (await user.matchPassword(req.body.password)) {
+        console.log(req.body.type);
+       var token=generateToken(user._id,req.body.type,user.email);
+       console.log(token);
+      res.status(200).send({ auth: true, token: token });
+      } else {
+        return  res.status(400).json({
+          message: "Invalid email or password",
         });
       }
-      // console.log(user);
-      else if (user) {
-        if (await user.matchPassword(req.body.password)) {
-          res.json({
-            email,
-            password,
-          });
-        } else {
-          res.json({
-            message: "Invalid email or password",
-          });
-        }
-      }
-      break;
-    case myEnum.CANDIDATE.value:
-      user = await Candidate.findOne({
-        email,
-      });
-      break;
-    case myEnum.QUIZMASTER.value:
-      user = await QuizMaster.findOne({
-       email,
-      });
-       console.log(user);
-      break;
-    default:
-      throw console.error("user doesn't exist");
-  }
-  if (!user) {
-    res.json({
-      message: "user doesn't exist",
-    });
-  }
-
-  // console.log(user);
-  else if (user) {
-    if (!user.verified) {
-      res.json({
-        message: "Please verify your account",
-      });
-    } else if (await user.matchPassword(req.body.password)) {
-     const newtoken=await new tokenModel (
-        {
-          userId:user._id,
-          token :crypto.randomBytes(32).toString("hex"),
-          expiresAt: Date.now()+3600000
-        }
-        
-      )
-      newtoken.save()
-      res.json({
-        email,
-        password,
-        newtoken
-      });
-    } else {
-      res.json({
-        message: "Invalid email or password",
-      });
     }
-  }}catch(error){
-    res.json({
+  } catch (error) {
+    console.log(error);
+     return res.status(400).json({
       status: "FAILED",
       message: error.message,
     });
@@ -331,8 +327,7 @@ const login = asyncHandler(async (req, res) => {
 //logout 
 
 const logout = asyncHandler(async (req, res) => {
-  req.logout()
-  req.session.destroy()
+  res.status(200).send({ auth: false, token: null });
 
 })
 
@@ -342,6 +337,6 @@ module.exports = {
   registerCandidate,
   verifyOTP,
   resendverification,
-  login,
+  loginUser,
   logout
 };
--- routes/auth/authRoute.js
+++ routes/auth/authRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { sendPasswordLink, setNewPassword } = require("../../controllers/auth/resetPassword");
+//const { sendPasswordLink, setNewPassword } = require("../../controllers/auth/resetPassword");
 const {
   registerAdmin,
   registerCandidate,
@@ -17,9 +17,9 @@ router.route("/registerQuizMaster").post(registerQuizMaster);
 router.route("/verifyOTP").post(verifyOTP);
 router.route("/resendOtpVerificarion").post(resendverification);
 
-router.route("/sendpasswordlink/:typeUser").post(sendPasswordLink);
-router.route("/:id/:token").post(setNewPassword);
-router.route("/verifyToken/:id").post(verifyToken)
+// router.route("/sendpasswordlink").post(sendPasswordLink);
+// router.route("/:id/:token").post(setNewPassword);
+router.route("/verifyToken").get(verifyToken)
 router.route('/logout').get(logout)
 router.route('/loginUser').post(loginUser)
 
