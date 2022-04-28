1107647de8204b9bfd155347239d67a5310028a2
marwa <marwaannebi25@gmail.com>
2022-04-28 09:35:56 +0100

    commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const authRoute = require("./routes/auth/authRoute");
 const adminRoute = require("./routes/admin/adminRoute");
 const quizMasterRoute=require("./routes/quizMaster/quizMasterRoute")
+const candidateRoute=require("./routes/candidate/candidateRoute")
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
@@ -17,5 +18,6 @@ connectDB();
 app.use("/auth", authRoute);
 app.use("/admin", adminRoute);
 app.use("/quizmaster",quizMasterRoute)
+app.use("/candidate",candidateRoute)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
