c26a553c0ffedb132e2802d8ca7aff04490cbed2
hadil30 <hedilfares30@gmail.com>
2022-05-05 02:36:18 +0100

    sendnotification

--- server.js
+++ server.js
@@ -7,7 +7,55 @@ const authRoute = require("./routes/auth/authRoute");
 const adminRoute = require("./routes/admin/adminRoute");
 const quizMasterRoute=require("./routes/quizMaster/quizMasterRoute")
 const candidateRoute=require("./routes/candidate/candidateRoute")
+
+
+
+// socket 
+const {Server} =require("socket.io")
+
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id);
+  console.log("someonehas connected")
+  });
+
+  socket.on("sendNotification", ({ senderName, receiverName}) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName,
+      
+    });
+  });
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    console.log("someone has disconnected ")
+  });
+
+});
+io.listen(4000);
 // parse application/x-www-form-urlencoded
+app.set('socketio', io);
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json({
   verify:(req,res,buffer)=> req['rawBody']=buffer,
