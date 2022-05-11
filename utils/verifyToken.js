f8482660cddf7c0ca98a40befd1bf09a8500e72c
hadil30 <hedilfares30@gmail.com>
2022-05-11 12:50:21 +0100

    fixed

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -9,29 +9,22 @@ const verifToken = expressAsyncHandler(async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      console.log(token);
-      //decodes token id
-      console.log("hello");
       if (token) {
-
-
-
-        console.log("hii");
-         decoded = jwt.verify(
-          token,
-          process.env.JWT_SECRET,
-          function (err, decoded) {
-            console.log(decoded);
-            req.user = await User.findById(decoded.id).select("-password")
+         decoded = jwt.verify(token,process.env.JWT_SECRET)
+          const decoded=req.user = await User.findById(decoded.id).select("-password")
+          console.log(req.user)
           }
-        );
+        
+        next();
       }
-      next();
-    } catch (error) {
+
+     catch (error) {
       res.status(401);
       throw new Error("Not authorized, token failed");
-    }
-  }
+    }}
+    if (!token)
+       res.status(401).send("not token");
+  
 });
 module.exports =verifToken;
 
