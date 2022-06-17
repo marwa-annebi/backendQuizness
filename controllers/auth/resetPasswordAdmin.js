98e00a2c4d13db191c391484d2ac97786ae7e0e8
hadil30 <hedilfares30@gmail.com>
2022-06-17 01:41:07 +0100

    statistic

---
+++ controllers/auth/resetPasswordAdmin.js
@@ -0,0 +1,103 @@
+var mongoose = require("mongoose");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const myEnum = require("./enumUser");
+const Admin = require("../../models/users/adminModel");
+const Quizmaster = require("../../models/users/quizmasterModel");
+const sendEmail = require("../../mailer/sendEmail");
+const Candidate = require("../../models/users/candidateModel");
+
+const sendPasswordLinkAdmin = async (req, res, next) => {
+  const { email, type } = req.body;
+
+  //     const { error } = emailSchema.validate(req.body.email);
+  //      if (error)
+  //    return res.status(400).send({ message: error.details[0].message });
+  if (!email) {
+    res.status(422).send({ message: "please input your email !" });
+  }
+  let user;
+  switch (type) {
+    case myEnum.ADMIN.value:
+      user = await Admin.findOne({ email });
+      //console.log(user);
+      break;
+  }
+  if (!user) {
+    return res
+      .status(409)
+      .send({ message: "User with given email does not exist!" });
+  }
+  if (user) {
+    const resetToken = user.getResetPasswordToken();
+    console.log(resetToken);
+    await user.save();
+    // console.log(user);
+    const url = ` ${process.env.CLIENT_URL}setNewPassword/${user._id}/${resetToken}/${type}`;
+    await sendEmail(user.email, "reset password", url);
+    console.log(url);
+    res
+      .status(200)
+      .send({ message: "Password reset link sent to your email account" });
+  } else if (!user) {
+    res.status(400).send({ message: "email not exist" });
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+    next();
+  }
+};
+
+const confirmResetPassword = async (resetToken) => {
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  try {
+    const user = await Admin.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() },
+    });
+    console.log(user);
+    if (!user) return { message: "user not exist" };
+    return user;
+  } catch (err) {
+    return { message: "Internal Server Error" };
+  }
+};
+
+const setNewPasswordAdmin = async (req, res, next) => {
+  try {
+    let { password } = req.body;
+    let { type, id } = req.params;
+    let user = await confirmResetPassword(
+      req.params.resetToken,
+      req.params.type,
+      req.params.id
+    );
+    console.log(user);
+    if (user) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      switch (type) {
+        case myEnum.ADMIN.value:
+          let newAdmin = await Admin.findByIdAndUpdate(req.params.id, {
+            password: hashedPassword,
+          });
+          //console.log(user);
+          newAdmin.save();
+          break;
+      }
+      return res.status(201).send({
+        message: "Password Updated Success",
+      });
+    }
+    return res.status(400).send({ msg: "user not exist!" });
+  } catch (error) {
+    res.status(500).send({ message: error });
+    next(error);
+  }
+};
+
+module.exports = { sendPasswordLinkAdmin, setNewPasswordAdmin, confirmResetPassword };
---
+++ controllers/quizMaster/Statistic.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const Quiz = require("../../models/quizModel");
+const Candidate = require("../../models/users/candidateModel");
+const nbQuizTotal = async (req, res) => {
+  let nbQuizRandomize = 0;
+  let nbQuizBySelection = 0;
+
+  const quiz = await Quiz.find({ quizmaster: req.user._id });
+  // console.log(quiz)
+  console.log(quiz);
+  if (quiz) {
+    for (let index = 0; index < quiz.length; index++) {
+      if (quiz[index].typeQuiz == "selection") {
+        nbQuizBySelection++;
+        // console.log(nbQuizBySelection);
+      } else {
+        nbQuizRandomize++;
+      }
+    }
+    res.status(200).send({
+      nbQuizBySelection,
+      nbQuizRandomize,
+    });
+  }
+  if (!quiz) {
+    res.status(400).send({ message: " quiz doesn't exist" });
+  }
+};
+const nbcandidatByeachMonth = async (req, res) => {
+  const { id } = req.body;
+  let array = [
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+    { month: {}, total: 0 },
+  ];
+
+  var today = new Date();
+
+  try {
+    await Candidate.aggregate([
+      ({
+        $match: {
+          //"author": { "$in": userIds }
+          quizmaster: [{ $lt:id }],
+          createdAt: {
+            $lt: today.toISOString(),
+          },
+        },
+      },
+      {
+        $project: {
+          month: {
+            $month: "$createdAt",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $month: "$createdAt",
+          },
+
+          total: {
+            $sum: 1,
+          },
+        },
+      }),
+    ]).exec(function (err, result) {
+      console.log(result);
+      // if (err) return handleError(err);
+      console.log("hello");
+      array.map((index, key) => {
+        for (let index = 0; index < result.length; index++) {
+          // let id2 = result[index]._id;
+          if (key == result[index]._id) {
+            var element = result[index];
+            // element += array[id2 - 1];
+            array[result[index]._id - 1] = element;
+          } else {
+            console.log("falseee");
+          }
+        }
+      });
+      return res.status(200).send(array);
+    });
+
+    // console.log(nb);
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { nbQuizTotal, nbcandidatByeachMonth };
